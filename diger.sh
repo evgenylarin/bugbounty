8faad7597ca8409ec4ef3e4a53e900f56409071d
Evgeny Larin <38251850+evgenylarin@users.noreply.github.com>
2019-02-22 15:05:03 +0300

    Update diger.sh
    Fix incorrect output

--- diger.sh
+++ diger.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Scan with amass
 # Put your scan results as a file
-for domain in $(cat example.txt);
-        do dig -4 +short $domain | tee -a  hosts.txt;
-done
 # Grep for a valid results and save into hosts.clean
-grep -v -E '[0-9.]+' hosts.txt && sort -u hosts.txt -o hosts.clean;
 # Scan with masscan from the file hosts.clean...
+for domain in $(cat example.txt);
+    do dig -4 +short $domain | tee -a  hosts.txt;
+    grep -v -E -q '[a-zA-Z]+' hosts.txt;
+    sort -u hosts.txt -o hosts.clean;
+done
